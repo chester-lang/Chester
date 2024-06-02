da27487cf4b285a46b1c6b854059eb727ceea8e0
澪 <mio@mio19.uk>
2024-06-02 20:13:25 +0800

    skeleton

---
+++ Chester Lang/src/main/scala/Parser.scala
@@ -0,0 +1,71 @@
+package chester.lang
+
+import fastparse._
+import NoWhitespace._
+
+object Parser {
+
+  def parseExpression(input: String): Parsed[Expr] = parse(input, expr(_))
+
+  def parseExpressions(input: String): Parsed[Seq[Expr]] = parse(input, program(_))
+
+  private def ws[$: P]: P[Unit] = P(CharsWhileIn(" \t\r\n").rep)
+
+  private def location[$: P]: P[SourceLocation] = P(Index).map(idx => SourceLocation("filename.scala", idx, idx))
+
+  private def string[$: P]: P[StringExpr] = P(location ~ "\"" ~/ CharsWhile(_ != '"').! ~ "\"").map {
+    case (loc, value) => StringExpr(value, loc)
+  }
+
+  private def table[$: P]: P[TableExpr] = P(location ~ "{" ~/ ws ~ keyValue.rep(sep = P(";") | ws) ~ ws ~ "}").map {
+    case (loc, entries) => TableExpr(entries.toMap, loc)
+  }
+
+  private def keyValue[$: P]: P[(String, Expr)] = P(identifier ~ ws ~ "=" ~ ws ~ expr).map {
+    case (key, value) => (key, value)
+  }
+
+  private def list[$: P]: P[ListExpr] = P(location ~ "[" ~/ ws ~ expr.rep(sep = P(",") ~ ws) ~ ws ~ "]").map {
+    case (loc, elements) => ListExpr(elements, loc)
+  }
+
+  private def functionCall[$: P]: P[FunctionCall] = P(location ~ identifier ~ "(" ~/ ws ~ expr.rep(sep = P(",") ~ ws) ~ ws ~ ")").map {
+    case (loc, name, args) => FunctionCall(name, args, loc)
+  }
+
+  private def infixExpr[$: P]: P[Expr] = P(expr ~ ws ~ operator ~ ws ~ expr).map {
+    case (left, op, right) => InfixExpr(left, op, right, left.location)
+  }
+
+  private def methodCall[$: P]: P[MethodCall] = P(location ~ expr ~ "." ~ identifier ~ "(" ~/ ws ~ expr.rep(sep = P(",") ~ ws) ~ ws ~ ")").map {
+    case (loc, target, method, args) => MethodCall(target, method, args, loc)
+  }
+
+  private def lambda[$: P]: P[LambdaExpr] = P(location ~ "(" ~ ws ~ params ~ ws ~ ")" ~ ws ~ "=>" ~ ws ~ expr).map {
+    case (loc, params, body) => LambdaExpr(params, body, loc)
+  }
+
+  private def params[$: P]: P[Seq[(String, Option[String])]] = P(param.rep(sep = P(",") ~ ws))
+
+  private def param[$: P]: P[(String, Option[String])] = P(identifier ~ ws ~ (":" ~ ws ~ identifier).?)
+
+  private def typeAnnotation[$: P]: P[TypeAnnotation] = P(location ~ expr ~ ":" ~ ws ~ identifier).map {
+    case (loc, expr, tpe) => TypeAnnotation(expr, tpe, loc)
+  }
+
+  private def identifier[$: P]: P[String] = P(CharIn("a-zA-Z") ~ CharsWhileIn("a-zA-Z0-9").rep.!)
+  
+  private def operator[$: P]: P[String] = P(CharIn("+-*/").!)
+
+  private def expr[$: P]: P[Expr] = P(
+    ws ~ (
+      table | list | string | functionCall | methodCall | lambda | typeAnnotation | infixExpr | simpleExpr
+      ) ~ ws
+  )
+
+  private def simpleExpr[$: P]: P[Expr] = P(location ~ identifier).map {
+    case (loc, id) => StringExpr(id, loc)  // Simplified; you may want to handle different types
+  }
+
+  private def program[$: P]: P[Seq[Expr]] = P(expr.rep(sep = P(";") | ws))
+}
---
+++ Chester Lang/src/test/scala/ParserSpec.scala
@@ -0,0 +1,63 @@
+package chester.lang
+
+import fastparse._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+class ParserSpec extends AnyFlatSpec with Matchers {
+
+  "Parser" should "parse a string expression correctly" in {
+    val input = "\"Hello, World!\""
+    val result = Parser.parseExpression(input)
+    result match {
+      case Parsed.Success(StringExpr(value, _), _) =>
+        value should be ("Hello, World!")
+      case _ => fail("Parsing failed")
+    }
+  }
+
+  it should "parse a table expression correctly" in {
+    val input = "{ x = 1; y = 2 }"
+    val result = Parser.parseExpression(input)
+    result match {
+      case Parsed.Success(TableExpr(entries, _), _) =>
+        entries.keys should contain allOf ("x", "y")
+      case _ => fail("Parsing failed")
+    }
+  }
+
+  it should "parse a list expression correctly" in {
+    val input = "[1, 2, 3]"
+    val result = Parser.parseExpression(input)
+    result match {
+      case Parsed.Success(ListExpr(elements, _), _) =>
+        elements.length should be (3)
+      case _ => fail("Parsing failed")
+    }
+  }
+
+  it should "parse multiple expressions correctly" in {
+    val input =
+      """
+        |{
+        |  x = 1;
+        |  y = 2
+        |}
+        |print("Hello, World!");
+        |[1, 2, 3];
+        |f(1, 2);
+        |x + y;
+        |x.f();
+        |x f y;
+        |(x, y) => x + y;
+        |x: Int
+        |""".stripMargin
+
+    val result = Parser.parseExpressions(input)
+    result match {
+      case Parsed.Success(expressions, _) =>
+        expressions.length should be (9)
+      case _ => fail("Parsing failed")
+    }
+  }
+}
