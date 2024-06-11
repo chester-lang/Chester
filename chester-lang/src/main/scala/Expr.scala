7c8b576e84594ec6277c2d9d0730bcc521009a22
澪 <mio@mio19.uk>
2024-06-11 19:50:57 +0800

    Expr?

---
+++ chester-lang/src/main/scala/Expr.scala
@@ -0,0 +1,26 @@
+package chester.lang
+
+sealed trait Expr {
+  def location: Option[SourceLocation]
+  def isWHNF: Boolean = false
+}
+
+
+case class TableExpr(location: Option[SourceLocation], entries: List[TableEntry]) extends Expr
+
+
+sealed trait TableEntry {
+  def isWHNF: Boolean = false
+}
+
+case class SimpleTableEntry(key: Expr, value: Expr) extends TableEntry
+
+case class PrimitiveExpr(location: Option[SourceLocation], name: String) extends Expr
+
+case class StringExpr(location: Option[SourceLocation], value: String) extends Expr
+
+case class IntegerExpr(location: Option[SourceLocation], value: BigInt) extends Expr
+
+case class CallExpr(location: Option[SourceLocation], callee: Expr, list: List[Expr]) extends Expr
+
+case class MacroCallExpr(location: Option[SourceLocation], callee: Expr, list: List[Expr]) extends Expr
