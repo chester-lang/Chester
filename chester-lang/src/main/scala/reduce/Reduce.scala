16880f8cbf961ac081955740ed74a315074ee645
澪 <mio@mio19.uk>
2024-06-13 20:09:16 +0800

    up

--- chester-lang/src/main/scala/reduce/Reduce.scala
+++ chester-lang/src/main/scala/reduce/Reduce.scala
@@ -1,9 +1,18 @@
 package chester.lang.reduce
 
 import chester.lang.Context
+import chester.lang.ast.AST
 import chester.lang.term.Term
+
+case class RuntimeError()
+
+type Result[A] = Either[RuntimeError, A]
 
 object Reduce {
   def apply(context: Context, expr: Term): Term = expr match
     case _ => ???
 }
+
+
+
+def parseAST(context: Context, astInTerm: Term): Result[AST] = ???
