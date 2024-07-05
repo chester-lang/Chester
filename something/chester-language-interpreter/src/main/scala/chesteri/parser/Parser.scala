caf280fa25e68828a93966ca786c516065d5c418
澪 <mio@mio19.uk>
2024-07-05 14:51:59 +1000

    up

---
+++ something/chester-language-interpreter/src/main/scala/chesteri/parser/Parser.scala
@@ -0,0 +1,29 @@
+package chesteri.parser;
+
+import fastparse.*
+import NoWhitespace.*
+import java.lang.Character.{isDigit, isLetter}
+
+case class ParserState(
+        fileName: String = "",
+        infixs: InfixDefitions = InfixDefitions())
+case class PrecedenceGroup(
+        name: String,
+        higherThan: Vector[PrecedenceGroup] = Vector(),
+        lowerThan: Vector[PrecedenceGroup] = Vector(),
+associativity: Associativity = Associativity.None,
+)
+enum Associativity {
+    case None
+    case Left
+    case Right
+}
+case class Prefix(name: String)
+case class Postfix(name: String)
+case class Infix(name: String, group: PrecedenceGroup)
+case class InfixDefitions(prefixes: Vector[Prefix] = Vector(),
+                          postfixes: Vector[Postfix] = Vector(),
+                          infixes: Vector[Infix] = Vector())
+
+case class Parser(state: ParserState) {
+}
