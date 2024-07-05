c95c639946a3ae84b7381b990eb074982799747b
澪 <mio@mio19.uk>
2024-07-06 00:05:55 +1000

    up

--- something/chester-language-interpreter/src/main/scala/chester/syntax/concrete/Expr.scala
+++ something/chester-language-interpreter/src/main/scala/chester/syntax/concrete/Expr.scala
@@ -13,55 +13,55 @@ sealed trait Expr extends WithPos {
 sealed trait Salt
 
 
-case class Identifier(name: String) extends Expr
+case class Identifier(sourcePos: Option[SourcePos], name: String) extends Expr
 
 // infix prefix postfix
-case class BinOpSeq(seq: Seq[Expr]) extends Expr with Salt {
+case class BinOpSeq(sourcePos: Option[SourcePos], seq: Seq[Expr]) extends Expr with Salt {
   override def descent(operator: Expr => Expr): Expr = {
-    BinOpSeq(seq.map(_.descentAndApply(operator)))
+    BinOpSeq(sourcePos, seq.map(_.descentAndApply(operator)))
   }
 }
 
-case class Infix(op: Expr, left: Expr, right: Expr) extends Expr {
+case class Infix(sourcePos: Option[SourcePos], op: Expr, left: Expr, right: Expr) extends Expr {
   override def descent(operator: Expr => Expr): Expr = {
-    Infix(op.descentAndApply(operator), left.descentAndApply(operator), right.descentAndApply(operator))
+    Infix(sourcePos, op.descentAndApply(operator), left.descentAndApply(operator), right.descentAndApply(operator))
   }
 }
 
-case class Prefix(op: Expr, operand: Expr) extends Expr {
+case class Prefix(sourcePos: Option[SourcePos], op: Expr, operand: Expr) extends Expr {
   override def descent(operator: Expr => Expr): Expr = {
-    Prefix(op.descentAndApply(operator), operand.descentAndApply(operator))
+    Prefix(sourcePos, op.descentAndApply(operator), operand.descentAndApply(operator))
   }
 }
 
-case class Postfix(op: Expr, operand: Expr) extends Expr {
+case class Postfix(sourcePos: Option[SourcePos], op: Expr, operand: Expr) extends Expr {
   override def descent(operator: Expr => Expr): Expr = {
-    Postfix(op.descentAndApply(operator), operand.descentAndApply(operator))
+    Postfix(sourcePos, op.descentAndApply(operator), operand.descentAndApply(operator))
   }
 }
 
-case class Block(heads: Vector[Expr], tail: Expr) extends Expr {
+case class Block(sourcePos: Option[SourcePos], heads: Vector[Expr], tail: Expr) extends Expr {
   override def descent(operator: Expr => Expr): Expr = {
-    Block(heads.map(_.descentAndApply(operator)), tail.descentAndApply(operator))
+    Block(sourcePos, heads.map(_.descentAndApply(operator)), tail.descentAndApply(operator))
   }
 }
 
-case class MacroCall(macroName: Expr, args: Vector[Expr]) extends Expr {
+case class MacroCall(sourcePos: Option[SourcePos], macroName: Expr, args: Vector[Expr]) extends Expr {
   override def descent(operator: Expr => Expr): Expr = {
-    MacroCall(macroName.descentAndApply(operator), args.map(_.descentAndApply(operator)))
+    MacroCall(sourcePos, macroName.descentAndApply(operator), args.map(_.descentAndApply(operator)))
   }
 }
 
-case class FunctionCall(function: Expr, implicitArgs: Vector[Expr], args: Vector[Expr]) extends Expr {
+case class FunctionCall(sourcePos: Option[SourcePos], function: Expr, implicitArgs: Vector[Expr], args: Vector[Expr]) extends Expr {
   def hasImplicitArgs: Boolean = implicitArgs.nonEmpty
 
   override def descent(operator: Expr => Expr): Expr = {
-    FunctionCall(function.descentAndApply(operator), implicitArgs.map(_.descentAndApply(operator)), args.map(_.descentAndApply(operator)))
+    FunctionCall(sourcePos, function.descentAndApply(operator), implicitArgs.map(_.descentAndApply(operator)), args.map(_.descentAndApply(operator)))
   }
 }
 
-case class IntegerLiteral(value: Int) extends Expr
+case class IntegerLiteral(sourcePos: Option[SourcePos], value: Int) extends Expr
 
-case class StringLiteral(value: String) extends Expr
+case class StringLiteral(sourcePos: Option[SourcePos], value: String) extends Expr
 
-case class VectorExpr(terms: Vector[Expr]) extends Expr
+case class VectorExpr(sourcePos: Option[SourcePos], terms: Vector[Expr]) extends Expr
