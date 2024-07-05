d69686745111b891e119947e0ddb45ff7a8c889d
澪 <mio@mio19.uk>
2024-07-05 21:14:50 +1000

    Precedence

--- something/chester-language-interpreter/src/main/scala/chesteri/parser/Accociativity.scala
+++ something/chester-language-interpreter/src/main/scala/chesteri/parser/Accociativity.scala
@@ -7,6 +7,12 @@ case class PrecedenceGroup(
                             associativity: Associativity = Associativity.None,
                           )
 
+val DefinePrecedenceGroup = PrecedenceGroup("Define")
+
+val TypePrecedenceGroup = PrecedenceGroup("Type", higherThan = Vector(DefinePrecedenceGroup))
+
+val DefaultPrecedenceGroup = PrecedenceGroup("Default", higherThan = Vector(TypePrecedenceGroup))
+
 enum Associativity {
   case None
   case Left
@@ -17,8 +23,17 @@ case class Prefix(name: String)
 
 case class Postfix(name: String)
 
-case class Infix(name: String, group: PrecedenceGroup)
+case class Infix(name: String, group: PrecedenceGroup = DefaultPrecedenceGroup)
+
+case class Mixfix(name: Vector[String], group: PrecedenceGroup = DefaultPrecedenceGroup)
 
 case class InfixDefitions(prefixes: Vector[Prefix] = Vector(),
                           postfixes: Vector[Postfix] = Vector(),
-                          infixes: Vector[Infix] = Vector())
+                          infixes: Vector[Infix] = Vector(),
+                          mixfixes: Vector[Mixfix] = Vector())
+
+
+val defaultInfixDefitions = InfixDefitions(infixes = Vector(
+  Infix("=", DefinePrecedenceGroup),
+  Infix(":", TypePrecedenceGroup),
+))
