9429588dc00fa20996fb42d6112cedc40ec228a9
澪 <mio@mio19.uk>
2024-07-08 00:14:10 +1000

    CharactersWhile

--- something/chester-language-interpreter/src/main/scala/chester/utils/parse/Parse.scala
+++ something/chester-language-interpreter/src/main/scala/chester/utils/parse/Parse.scala
@@ -8,10 +8,13 @@ import java.lang.Character.{isDigit, isLetter}
 type Character = Int
 
 
-def CharacterPred(p: Character => Boolean)(implicit ctx: P[_]): P[Unit] =
-  P(CharPred(c => (!Character.isSurrogate(c) && p(c.toInt))) |
+inline def CharacterPred(inline p: Character => Boolean)(implicit ctx: P[_]): P[Unit] =
+  CharPred(c => (!Character.isSurrogate(c) && p(c.toInt))) |
     (CharPred(Character.isHighSurrogate) ~ CharPred(Character.isLowSurrogate)).!.flatMap(c => {
       val codePoint = Character.codePointAt(c, 0)
       assert(c.codePointCount(0, c.length) == 1)
       if (p(codePoint)) Pass else Fail
-    }))
+    })
+
+inline def CharactersWhile(inline p: Character => Boolean, inline min: Int = 1)(implicit ctx: P[_]): P[Unit] =
+  CharacterPred(p).rep(min)
