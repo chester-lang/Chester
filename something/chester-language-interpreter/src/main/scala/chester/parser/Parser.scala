8b8f72a5bbcd804196b789a3f2bb02a36ae7234f
澪 <mio@mio19.uk>
2024-07-17 19:49:15 +1200

    something

--- something/chester-language-interpreter/src/main/scala/chester/parser/Parser.scala
+++ something/chester-language-interpreter/src/main/scala/chester/parser/Parser.scala
@@ -2,10 +2,19 @@ package chester.parser;
 
 import fastparse.*
 import NoWhitespace.*
+import chester.utils.parse._
+
 import java.lang.Character.{isDigit, isLetter}
 
 case class ParserState(
                         fileName: String = "")
 
 case class Parser(state: ParserState) {
+  val ASCIIAllowedSymbols = "-=_+\\|;:,.<>/?`~!@$%^&*"
+  val ReservedSymbols = "#()[]{}'\""
+  def identifierFirst(x: Character) = isLetter(x)
+
+  def identifierRest(x: Character) = isLetter(x) || isDigit(x)
+
+  def identifier[$: P]: P[String] = P((CharacterPred(identifierFirst).rep(1) ~ CharacterPred(identifierRest).rep).!)
 }
