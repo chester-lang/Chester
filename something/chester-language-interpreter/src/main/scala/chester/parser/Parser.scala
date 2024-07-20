e55f89ba707cfb2d3ebe1640680d380e72669832
澪 <mio@mio19.uk>
2024-07-20 05:46:45 +0000

    up

--- something/chester-language-interpreter/src/main/scala/chester/parser/Parser.scala
+++ something/chester-language-interpreter/src/main/scala/chester/parser/Parser.scala
@@ -2,7 +2,9 @@ package chester.parser;
 
 import fastparse.*
 import NoWhitespace.*
-import chester.utils.parse._
+import chester.error.SourcePos
+import chester.utils.StringIndex
+import chester.utils.parse.*
 
 import java.lang.Character.{isDigit, isLetter}
 
@@ -17,4 +19,16 @@ case class Parser(state: ParserState) {
   def identifierRest(x: Character) = isLetter(x) || isDigit(x)
 
   def identifier[$: P]: P[String] = P((CharacterPred(identifierFirst).rep(1) ~ CharacterPred(identifierRest).rep).!)
+
+
+  private def begin[$: P]: P[Int] = Index
+  private def end[$: P]: P[Int] = Index
+
+
+  private def loc(begin: Int, end: Int)(implicit ctx: P[?]): Option[SourcePos] = {
+    val index = StringIndex(ctx.input.slice(0, ctx.input.length))
+    ???
+  }
+
+
 }
