895128efc3ecb27bc65bcbbd82b77e3dfc673778
澪 <mio@mio19.uk>
2024-07-21 22:11:41 +1200

    up

--- something/chester-language-interpreter/src/test/scala/chester/parser/ParserTest.scala
+++ something/chester-language-interpreter/src/test/scala/chester/parser/ParserTest.scala
@@ -2,7 +2,7 @@ package chester.parser
 
 import munit.FunSuite
 import fastparse.*
-import chester.syntax.concrete.{Expr, Identifier}
+import chester.syntax.concrete._
 
 class ParserTest extends FunSuite {
 
@@ -33,5 +33,86 @@ class ParserTest extends FunSuite {
   test("parse empty input") {
     val result = Parser.parseExpression("testFile", "")
     assert(result.isInstanceOf[Parsed.Failure])
+  }
+
+
+  def parseAndCheck(input: String, expected: Expr): Unit = {
+    val result = Parser.parseExpression("testFile", input)
+    result match {
+      case Parsed.Success(value, _) =>
+        value match {
+          case IntegerLiteral(actualValue, _) =>
+            expected match {
+              case IntegerLiteral(expectedValue, _) =>
+                assertEquals(actualValue, expectedValue, s"Failed for input: $input")
+              case _ => fail(s"Expected IntegerLiteral but got ${value.getClass} for input: $input")
+            }
+          case DoubleLiteral(actualValue, _) =>
+            expected match {
+              case DoubleLiteral(expectedValue, _) =>
+                assertEquals(actualValue, expectedValue, s"Failed for input: $input")
+              case _ => fail(s"Expected DoubleLiteral but got ${value.getClass} for input: $input")
+            }
+          case _ => fail(s"Unexpected expression type for input: $input")
+        }
+      case _ => fail(s"Parsing failed for input: $input")
+    }
+  }
+
+  // Tests for IntegerLiteral
+  test("parse valid decimal integer") {
+    val input = "12345"
+    val expected = IntegerLiteral(BigInt("12345"))
+    parseAndCheck(input, expected)
+  }
+
+  test("parse valid hexadecimal integer") {
+    val input = "0x1A3F"
+    val expected = IntegerLiteral(BigInt("1A3F", 16))
+    parseAndCheck(input, expected)
+  }
+
+  test("parse valid binary integer") {
+    val input = "0b1101"
+    val expected = IntegerLiteral(BigInt("1101", 2))
+    parseAndCheck(input, expected)
+  }
+
+  test("parse signed integer") {
+    val input = "-6789"
+    val expected = IntegerLiteral(BigInt("-6789"))
+    parseAndCheck(input, expected)
+  }
+
+  // Tests for DoubleLiteral
+  test("parse valid double with exponent") {
+    val input = "3.14e2"
+    val expected = DoubleLiteral(BigDecimal("3.14e2"))
+    parseAndCheck(input, expected)
+  }
+
+  test("parse signed double with exponent") {
+    val input = "-1.23e-4"
+    val expected = DoubleLiteral(BigDecimal("-1.23e-4"))
+    parseAndCheck(input, expected)
+  }
+
+  test("parse double without exponent") {
+    val input = "456.789"
+    val expected = DoubleLiteral(BigDecimal("456.789"))
+    parseAndCheck(input, expected)
+  }
+
+  // General literal tests
+  test("parse integerLiteral") {
+    val input = "12345"
+    val expected = IntegerLiteral(BigInt("12345"))
+    parseAndCheck(input, expected)
+  }
+
+  test("parse doubleLiteral") {
+    val input = "1.23e4"
+    val expected = DoubleLiteral(BigDecimal("1.23e4"))
+    parseAndCheck(input, expected)
   }
 }
