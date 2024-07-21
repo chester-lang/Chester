7f99ee2796fd504ab4c482c669094aa9c7269031
澪 <mio@mio19.uk>
2024-07-21 21:47:23 +1200

    up

--- something/chester-language-interpreter/src/main/scala/chester/parser/Parser.scala
+++ something/chester-language-interpreter/src/main/scala/chester/parser/Parser.scala
@@ -3,36 +3,44 @@ package chester.parser;
 import fastparse.*
 import NoWhitespace.*
 import chester.error.{Pos, RangeInFile, SourcePos}
-import chester.syntax.concrete.Expr
+import chester.syntax.concrete.{Expr, Identifier}
 import chester.utils.StringIndex
 import chester.utils.parse.*
 
 import java.lang.Character.{isDigit, isLetter}
 
 case class ParserInternal(fileName: String)(implicit ctx: P[?]) {
-  val ASCIIAllowedSymbols = "-=_+\\|;:,.<>/?`~!@$%^&*"
+  val ASCIIAllowedSymbols = "-=_+\\|;:,.<>/?`~!@$%^&*".toSet.map(_.toInt)
   val ReservedSymbols = "#()[]{}'\""
-  def identifierFirst(x: Character) = isLetter(x)
 
-  def identifierRest(x: Character) = isLetter(x) || isDigit(x)
+  def isSymbol(x: Character) = ASCIIAllowedSymbols.contains(x)
 
-  def identifier: P[String] = P((CharacterPred(identifierFirst).rep(1) ~ CharacterPred(identifierRest).rep).!)
+  def identifierFirst(x: Character) = isLetter(x) || isSymbol(x)
 
+  def identifierRest(x: Character) = identifierFirst(x) || isDigit(x)
 
-  private def begin: P[Int] = Index
-  private def end: P[Int] = Index
+  def id: P[String] = P((CharacterPred(identifierFirst).rep(1) ~ CharacterPred(identifierRest).rep).!)
+
+  def begin: P[Int] = Index
+
+  def end: P[Int] = Index
 
 
   val index = StringIndex(ctx.input.slice(0, ctx.input.length))
 
-  private def loc(begin: Int, end: Int): Option[SourcePos] = {
+  private def loc(begin: Int, end: Int): SourcePos = {
     val start = index.charIndexToUnicodeLineAndColumn(begin)
     val endPos = index.charIndexToUnicodeLineAndColumn(end)
-    Some(SourcePos(fileName, RangeInFile(Pos(begin, start.line, start.column), Pos(end, endPos.line, endPos.column))))
+    SourcePos(fileName, RangeInFile(Pos(begin, start.line, start.column), Pos(end, endPos.line, endPos.column)))
   }
-  
-  def apply: P[Expr] = ???
 
+  extension [T](inline parse0: P[T]) {
+    inline def withPos: P[(T, SourcePos)] = (begin ~ parse0 ~ end).map { case (b, x, e) => (x, loc(b, e)) }
+  }
+
+  def identifier: P[Expr] = P(id.withPos).map { case (name, pos) => Identifier(Some(pos), name) }
+
+  def apply: P[Expr] = identifier // | ...
 
 }
 
