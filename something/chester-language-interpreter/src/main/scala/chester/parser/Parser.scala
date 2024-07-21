8f9e06aa746440013c0d425966cad4b646888a8b
澪 <mio@mio19.uk>
2024-07-21 15:55:23 +1200

    ref

--- something/chester-language-interpreter/src/main/scala/chester/parser/Parser.scala
+++ something/chester-language-interpreter/src/main/scala/chester/parser/Parser.scala
@@ -2,33 +2,37 @@ package chester.parser;
 
 import fastparse.*
 import NoWhitespace.*
-import chester.error.SourcePos
+import chester.error.{Pos, RangeInFile, SourcePos}
 import chester.utils.StringIndex
 import chester.utils.parse.*
 
 import java.lang.Character.{isDigit, isLetter}
 
-case class ParserState(
-                        fileName: String = "")
-
-case class Parser(state: ParserState) {
+case class ParserInternal(fileName: String)(implicit ctx: P[?]) {
   val ASCIIAllowedSymbols = "-=_+\\|;:,.<>/?`~!@$%^&*"
   val ReservedSymbols = "#()[]{}'\""
   def identifierFirst(x: Character) = isLetter(x)
 
   def identifierRest(x: Character) = isLetter(x) || isDigit(x)
 
-  def identifier[$: P]: P[String] = P((CharacterPred(identifierFirst).rep(1) ~ CharacterPred(identifierRest).rep).!)
+  def identifier: P[String] = P((CharacterPred(identifierFirst).rep(1) ~ CharacterPred(identifierRest).rep).!)
 
 
-  private def begin[$: P]: P[Int] = Index
-  private def end[$: P]: P[Int] = Index
+  private def begin: P[Int] = Index
+  private def end: P[Int] = Index
 
 
-  private def loc(begin: Int, end: Int)(implicit ctx: P[?]): Option[SourcePos] = {
-    val index = StringIndex(ctx.input.slice(0, ctx.input.length))
-    ???
+  val index = StringIndex(ctx.input.slice(0, ctx.input.length))
+
+  private def loc(begin: Int, end: Int): Option[SourcePos] = {
+    val start = index.charIndexToUnicodeLineAndColumn(begin)
+    val endPos = index.charIndexToUnicodeLineAndColumn(end)
+    Some(SourcePos(fileName, RangeInFile(Pos(begin, start.line, start.column), Pos(end, endPos.line, endPos.column))))
   }
 
 
+}
+
+object Parser {
+  
 }
