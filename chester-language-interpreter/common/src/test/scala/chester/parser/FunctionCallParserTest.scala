e5cb08cb5c5b7ada8a8e7ae38df2e95489fc6d67
澪 <mio@mio19.uk>
2024-07-29 18:16:13 +1200

    refact on ParsedExpr - FunctionCallParserTest

--- chester-language-interpreter/common/src/test/scala/chester/parser/FunctionCallParserTest.scala
+++ chester-language-interpreter/common/src/test/scala/chester/parser/FunctionCallParserTest.scala
@@ -1,7 +1,6 @@
 package chester.parser
 
 import munit.FunSuite
-import fastparse.*
 import chester.syntax.concrete._
 import chester.parser._
 
@@ -9,41 +8,34 @@ class FunctionCallParserTest extends FunSuite {
 
   test("parse simple function call with no arguments") {
     val input = "func()"
-    val expected = FunctionCall(Identifier("func"), Telescope(Vector()))
+    val expected = FunctionCall(Identifier("func"), Tuple(Vector()))
     parseAndCheck(input, expected)
   }
 
   test("parse function call with multiple arguments") {
     val input = "multiply(2, 3)"
-    val expected = FunctionCall(Identifier("multiply"), Telescope(Vector(Arg.of(IntegerLiteral(2)), Arg.of(IntegerLiteral(3)))))
+    val expected = FunctionCall(Identifier("multiply"), Tuple(Vector(IntegerLiteral(2), IntegerLiteral(3))))
     parseAndCheck(input, expected)
   }
 
   test("parse function call with multiple arguments and symbol identifier") {
     val input = "+(2, 3)"
-    val expected = FunctionCall(Identifier("+"), Telescope(Vector(Arg.of(IntegerLiteral(2)), Arg.of(IntegerLiteral(3)))))
+    val expected = FunctionCall(Identifier("+"), Tuple(Vector(IntegerLiteral(2), IntegerLiteral(3))))
     parseAndCheck(input, expected)
   }
 
   test("parse function call with mixed type arguments") {
     val input = "createPerson(\"John\", 30, true)"
-    val expected = FunctionCall(Identifier("createPerson"), Telescope(Vector(Arg.of(StringLiteral("John")), Arg.of(IntegerLiteral(30)), Arg.of(Identifier("true")))))
+    val expected = FunctionCall(Identifier("createPerson"), Tuple(Vector(StringLiteral("John"), IntegerLiteral(30), Identifier("true"))))
     parseAndCheck(input, expected)
   }
 
-  test("parse function call with mixed type arguments") {
+  test("parse function call with generics and mixed type arguments") {
     val input = "createPerson<Integer>(\"John\", 30, true)"
-    val expected = FunctionCall(FunctionCall(Identifier("createPerson"), Telescope(Vector(
-      Arg.of(
-        (
-          Identifier(
-            name = "Integer",
-            sourcePos = None
-          )
-          )
-      )), true)), Telescope(Vector(
-      Arg.of(StringLiteral("John")), Arg.of(IntegerLiteral(30)), Arg.of(Identifier("true")))))
+    val expected = FunctionCall(
+      FunctionCall(Identifier("createPerson"), Generics(Vector(Identifier("Integer")))),
+      Tuple(Vector(StringLiteral("John"), IntegerLiteral(30), Identifier("true")))
+    )
     parseAndCheck(input, expected)
   }
-
 }
