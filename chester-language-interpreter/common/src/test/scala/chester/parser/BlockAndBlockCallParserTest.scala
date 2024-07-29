26541ee997a631ba5f2dd84703283b52323590b0
澪 <mio@mio19.uk>
2024-07-29 18:32:26 +1200

    refact on ParsedExpr - BlockAndBlockCallParserTest

--- chester-language-interpreter/common/src/test/scala/chester/parser/BlockAndBlockCallParserTest.scala
+++ chester-language-interpreter/common/src/test/scala/chester/parser/BlockAndBlockCallParserTest.scala
@@ -1,20 +1,18 @@
 package chester.parser
 
 import munit.FunSuite
-import fastparse.*
 import chester.syntax.concrete._
 import chester.parser._
 
 class BlockAndBlockCallParserTest extends FunSuite {
-
   test("parse block with multiple statements without newlines") {
     val input = "{f(1); g(2); 3}"
     val expected = Block(
       Vector(
-        FunctionCall(Identifier("f"), Telescope.of(Arg.of(IntegerLiteral(1)))),
-        FunctionCall(Identifier("g"), Telescope.of(Arg.of(IntegerLiteral(2))))
+        FunctionCall(Identifier("f"), Tuple(Vector(IntegerLiteral(1)))),
+        FunctionCall(Identifier("g"), Tuple(Vector(IntegerLiteral(2))))
       ),
-      IntegerLiteral(3)
+      Some(IntegerLiteral(3))
     )
     parseAndCheck(input, expected)
   }
@@ -23,10 +21,10 @@ class BlockAndBlockCallParserTest extends FunSuite {
     val input = "{\n  f(1);\n  g(2);\n  3\n}"
     val expected = Block(
       Vector(
-        FunctionCall(Identifier("f"), Telescope.of(Arg.of(IntegerLiteral(1)))),
-        FunctionCall(Identifier("g"), Telescope.of(Arg.of(IntegerLiteral(2))))
+        FunctionCall(Identifier("f"), Tuple(Vector(IntegerLiteral(1)))),
+        FunctionCall(Identifier("g"), Tuple(Vector(IntegerLiteral(2))))
       ),
-      IntegerLiteral(3)
+      Some(IntegerLiteral(3))
     )
     parseAndCheck(input, expected)
   }
@@ -35,11 +33,11 @@ class BlockAndBlockCallParserTest extends FunSuite {
     val input = "f{g(1); 2}"
     val expected = FunctionCall(
       Identifier("f"),
-      Telescope.of(Arg.of(Block(
+      Tuple(Vector(Block(
         Vector(
-          FunctionCall(Identifier("g"), Telescope.of(Arg.of(IntegerLiteral(1))))
+          FunctionCall(Identifier("g"), Tuple(Vector(IntegerLiteral(1))))
         ),
-        IntegerLiteral(2)
+        Some(IntegerLiteral(2))
       )))
     )
     parseAndCheck(input, expected)
@@ -49,23 +47,24 @@ class BlockAndBlockCallParserTest extends FunSuite {
     val input = "f {\n  g(1);\n  2\n}"
     val expected = FunctionCall(
       Identifier("f"),
-      Telescope.of(Arg.of(Block(
+      Tuple(Vector(Block(
         Vector(
-          FunctionCall(Identifier("g"), Telescope.of(Arg.of(IntegerLiteral(1))))
+          FunctionCall(Identifier("g"), Tuple(Vector(IntegerLiteral(1))))
         ),
-        IntegerLiteral(2)
+        Some(IntegerLiteral(2))
       )))
     )
     parseAndCheck(input, expected)
   }
+
   test("parse function call with block as argument without newlines") {
     val input = "f(1) {2}"
     val expected = FunctionCall(
       FunctionCall(
         Identifier("f"),
-        Telescope.of(Arg.of(IntegerLiteral(1)))
+        Tuple(Vector(IntegerLiteral(1)))
       ),
-      Telescope.of(Arg.of(Block(Vector(), IntegerLiteral(2))))
+      Tuple(Vector(Block(Vector(), Some(IntegerLiteral(2)))))
     )
     parseAndCheck(input, expected)
   }
