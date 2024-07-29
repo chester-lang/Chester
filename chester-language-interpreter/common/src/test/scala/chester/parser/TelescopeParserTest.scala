16387c52fb93f9cf17a79c4f65ef76ff87dd2657
澪 <mio@mio19.uk>
2024-07-29 18:10:58 +1200

    refact on ParsedExpr - TelescopeParserTest

--- chester-language-interpreter/common/src/test/scala/chester/parser/TelescopeParserTest.scala
+++ chester-language-interpreter/common/src/test/scala/chester/parser/TelescopeParserTest.scala
@@ -1,142 +1,108 @@
 package chester.parser
+
 import munit.FunSuite
-import fastparse.*
+import fastparse._
 import chester.syntax.concrete._
 import chester.parser._
 
 class TelescopeParserTest extends FunSuite {
-
-  test("parse telescope with simple arguments") {
+  test("parse tuple with simple arguments") {
     val input = "(a, b, c)"
-    val expected = Telescope(Vector(
-      Arg(Vector.empty, None, None, Some(Identifier("a"))),
-      Arg(Vector.empty, None, None, Some(Identifier("b"))),
-      Arg(Vector.empty, None, None, Some(Identifier("c")))
+    val expected = Tuple(Vector(
+      Identifier("a"),
+      Identifier("b"),
+      Identifier("c")
     ))
     parseAndCheck(input, expected)
   }
 
-  test("parse telescope with simple arguments ending comma") {
+  test("parse tuple with simple arguments ending comma") {
     val input = "(a, b, c,)"
-    val expected = Telescope(Vector(
-      Arg(Vector.empty, None, None, Some(Identifier("a"))),
-      Arg(Vector.empty, None, None, Some(Identifier("b"))),
-      Arg(Vector.empty, None, None, Some(Identifier("c")))
+    val expected = Tuple(Vector(
+      Identifier("a"),
+      Identifier("b"),
+      Identifier("c")
     ))
     parseAndCheck(input, expected)
   }
 
-  test("parse telescope empty") {
+  test("parse tuple empty") {
     val input = "()"
-    val expected = Telescope(Vector(
-    ))
+    val expected = Tuple(Vector())
     parseAndCheck(input, expected)
   }
 
-  test("parse telescope with simple arguments with comments") {
+  test("parse tuple with simple arguments with comments") {
     val input = "(a, // comment \n b, c)"
-    val expected = Telescope(Vector(
-      Arg(Vector.empty, None, None, Some(Identifier("a"))),
-      Arg(Vector.empty, None, None, Some(Identifier("b"))),
-      Arg(Vector.empty, None, None, Some(Identifier("c")))
+    val expected = Tuple(Vector(
+      Identifier("a"),
+      Identifier("b"),
+      Identifier("c")
     ))
     parseAndCheck(input, expected)
   }
 
-  test("parse telescope with arguments having type") {
+  test("parse tuple with arguments having type") {
     val input = "(a: Integer)"
-    val expected = Telescope(Vector(
-      Arg(Vector.empty, Some(Identifier("a")), Some(Identifier("Integer")), None)
+    val expected = Tuple(Vector(
+      BinOpSeq(Vector(Identifier("a"), Identifier(":"), Identifier("Integer")))
     ))
     parseAndCheck(input, expected)
   }
 
-  test("parse telescope with arguments having types") {
+  test("parse tuple with arguments having types") {
     val input = "(a: Integer, b: String, c: Double)"
-    val expected = Telescope(Vector(
-      Arg(Vector.empty, Some(Identifier("a")), Some(Identifier("Integer")), None),
-      Arg(Vector.empty, Some(Identifier("b")), Some(Identifier("String")), None),
-      Arg(Vector.empty, Some(Identifier("c")), Some(Identifier("Double")), None)
+    val expected = Tuple(Vector(
+      BinOpSeq(Vector(Identifier("a"), Identifier(":"), Identifier("Integer"))),
+      BinOpSeq(Vector(Identifier("b"), Identifier(":"), Identifier("String"))),
+      BinOpSeq(Vector(Identifier("c"), Identifier(":"), Identifier("Double")))
     ))
     parseAndCheck(input, expected)
   }
 
-  test("parse telescope with arguments having default value") {
+  test("parse tuple with arguments having default value") {
     val input = "(a = 1)"
-    val expected = Telescope(Vector(
-      Arg(Vector.empty, Some(Identifier("a")), None, Some(IntegerLiteral(1)))
+    val expected = Tuple(Vector(
+      BinOpSeq(Vector(Identifier("a"), Identifier("="), IntegerLiteral(1)))
     ))
     parseAndCheck(input, expected)
   }
 
-  test("parse telescope with arguments having default values") {
+  test("parse tuple with arguments having default values") {
     val input = "(a = 1, b = 2, c = 3)"
-    val expected = Telescope(Vector(
-      Arg(Vector.empty, Some(Identifier("a")), None, Some(IntegerLiteral(1))),
-      Arg(Vector.empty, Some(Identifier("b")), None, Some(IntegerLiteral(2))),
-      Arg(Vector.empty, Some(Identifier("c")), None, Some(IntegerLiteral(3)))
+    val expected = Tuple(Vector(
+      BinOpSeq(Vector(Identifier("a"), Identifier("="), IntegerLiteral(1))),
+      BinOpSeq(Vector(Identifier("b"), Identifier("="), IntegerLiteral(2))),
+      BinOpSeq(Vector(Identifier("c"), Identifier("="), IntegerLiteral(3)))
     ))
     parseAndCheck(input, expected)
   }
 
-  test("parse telescope with arguments having types and default values") {
+  test("parse tuple with arguments having types and default values") {
     val input = "(a: Integer = 1, b: String = \"test\", c: Double = 3.14)"
-    val expected = Telescope(Vector(
-      Arg(Vector.empty, Some(Identifier("a")), Some(Identifier("Integer")), Some(IntegerLiteral(1))),
-      Arg(Vector.empty, Some(Identifier("b")), Some(Identifier("String")), Some(StringLiteral("test"))),
-      Arg(Vector.empty, Some(Identifier("c")), Some(Identifier("Double")), Some(DoubleLiteral(BigDecimal(3.14))))
+    val expected = Tuple(Vector(
+      BinOpSeq(Vector(Identifier("a"), Identifier(":"), Identifier("Integer"), Identifier("="), IntegerLiteral(1))),
+      BinOpSeq(Vector(Identifier("b"), Identifier(":"), Identifier("String"), Identifier("="), StringLiteral("test"))),
+      BinOpSeq(Vector(Identifier("c"), Identifier(":"), Identifier("Double"), Identifier("="), DoubleLiteral(BigDecimal(3.14))))
     ))
     parseAndCheck(input, expected)
   }
 
-  test("parse telescope with arguments having decorations") {
-    val input = "(@implicit a: Integer = 1, b: Integer, c)"
-    val expected = Telescope(Vector(
-      Arg(Vector(Identifier("implicit")), Some(Identifier("a")), Some(Identifier("Integer")), Some(IntegerLiteral(1))),
-      Arg(Vector.empty, Some(Identifier("b")), Some(Identifier("Integer")), None),
-      Arg(Vector.empty, None, None, Some(Identifier("c")))
-    ))
-    parseAndCheck(input, expected)
-  }
-
-  test("parse telescope with mixed arguments") {
-    val input = "(a, b: String, c = 3.14, @implicit d: Integer = 1)"
-    val expected = Telescope(Vector(
-      Arg(Vector.empty, None, None, Some(Identifier("a"))),
-      Arg(Vector.empty, Some(Identifier("b")), Some(Identifier("String")), None),
-      Arg(Vector.empty, Some(Identifier("c")), None, Some(DoubleLiteral(BigDecimal(3.14)))),
-      Arg(Vector(Identifier("implicit")), Some(Identifier("d")), Some(Identifier("Integer")), Some(IntegerLiteral(1)))
-    ))
-    parseAndCheck(input, expected)
-  }
-
-  test("parse telescope with arguments without names") {
+  test("parse tuple with arguments without names") {
     val input = "(1, 2, 3)"
-    val expected = Telescope(Vector(
-      Arg(Vector.empty, None, None, Some(IntegerLiteral(1))),
-      Arg(Vector.empty, None, None, Some(IntegerLiteral(2))),
-      Arg(Vector.empty, None, None, Some(IntegerLiteral(3)))
+    val expected = Tuple(Vector(
+      IntegerLiteral(1),
+      IntegerLiteral(2),
+      IntegerLiteral(3)
     ))
     parseAndCheck(input, expected)
   }
 
-  test("parse implicit telescope with arguments without names") {
+  test("parse generics with arguments without names") {
     val input = "<Integer>"
-    val expected =
-      Telescope(
-        args = Vector(
-          Arg.of(
-            (
-              Identifier(
-                name = "Integer",
-                sourcePos = None
-              )
-            )
-          )
-        ),
-        implicitly = true,
-        sourcePos = None
-      )
+    val expected = Generics(Vector(
+      Identifier("Integer")
+    ))
     parseAndCheck(input, expected)
   }
 }
