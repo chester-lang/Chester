341e5df7871d4769f3f3e51e04a13ebf67a1f963
澪 <mio@mio19.uk>
2024-07-31 16:49:41 +1200

    it is expressive

--- chester-language-interpreter/common/src/main/scala/chester/tyck/ExprTycker.scala
+++ chester-language-interpreter/common/src/main/scala/chester/tyck/ExprTycker.scala
@@ -9,7 +9,26 @@ case class LocalCtx()
 
 case class Judge(wellTyped: Term, ty: Term)
 
+case class TyckError(message: String)
+
+object TyckError {
+  val emptyResults = TyckError("Empty Results")
+}
+
+case class Results[T](xs: LazyList[Either[TyckError, T]]) {
+  def map[U](f: T => U): Results[U] = Results(xs.map(_.map(f)))
+
+  def flatMap[U](f: T => Results[U]): Results[U] = Results(xs.flatMap {
+    case Left(err) => LazyList(Left(err))
+    case Right(x) => f(x).xs
+  })
+
+  def getEither: Either[TyckError, T] = xs.find(_.isRight).getOrElse(xs.headOption.getOrElse(Left(TyckError.emptyResults)))
+}
+
+
 case class ExprTycker(state: TyckState, localCtx: LocalCtx) {
-  def inherit(expr: Expr, ty: Term): Judge = ???
-  def synthesize(expr: Expr): Judge = ???
+  def inherit(expr: Expr, ty: Term): Results[Judge] = ???
+
+  def synthesize(expr: Expr): Results[Judge] = ???
 }
