51be5da6e37a86c1006dac194c47d03cf5041fff
澪 <mio@mio19.uk>
2024-08-03 20:24:27 +1200

    up

--- chester-language-interpreter/common/src/main/scala/chester/utils/parse/Parse.scala
+++ chester-language-interpreter/common/src/main/scala/chester/utils/parse/Parse.scala
@@ -8,7 +8,7 @@ import java.lang.Character.{isDigit, isLetter}
 type Character = Int
 
 
-inline def CharacterPred(inline p: Character => Boolean)(implicit ctx: P[_]): P[Unit] =
+inline def CharacterPred(inline p: Character => Boolean)(implicit ctx: P[?]): P[Unit] =
   CharPred(c => (!Character.isSurrogate(c) && p(c.toInt))) |
     (CharPred(Character.isHighSurrogate) ~ CharPred(Character.isLowSurrogate)).!.flatMap(c => {
       val codePoint = Character.codePointAt(c, 0)
@@ -16,8 +16,8 @@ inline def CharacterPred(inline p: Character => Boolean)(implicit ctx: P[_]): P[
       if (p(codePoint)) Pass else Fail
     })
 
-inline def CharactersWhile(inline p: Character => Boolean, inline min: Int = 1)(implicit ctx: P[_]): P[Unit] =
+inline def CharactersWhile(inline p: Character => Boolean, inline min: Int = 1)(implicit ctx: P[?]): P[Unit] =
   CharacterPred(p).rep(min)
 
-inline def StringPred(inline p: String => Boolean)(implicit ctx: P[_]): P[Unit] =
+inline def StringPred(inline p: String => Boolean)(implicit ctx: P[?]): P[Unit] =
   AnyChar.rep.!.flatMap(c => if (p(c)) Pass else Fail)
