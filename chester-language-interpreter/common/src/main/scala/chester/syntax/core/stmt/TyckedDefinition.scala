e97ab46d46897360579ddc3cc6d81b13799cf572
澪 <mio@mio19.uk>
2024-08-04 20:34:31 +1200

    Do they look like api?

---
+++ chester-language-interpreter/common/src/main/scala/chester/syntax/core/stmt/TyckedDefinition.scala
@@ -0,0 +1,19 @@
+package chester.syntax.core.stmt
+
+import chester.syntax.concrete.{FilePath, QualifiedIDString}
+import chester.syntax.core.{Term, TermMeta}
+import chester.tyck.Judge
+
+case class TyckedModule(id: QualifiedIDString, files: Vector[TyckedModuleFile])
+case class TyckedModuleFile(fileName: FilePath, content: TyckedBlock)
+case class TyckedBlock(definitions: Vector[TyckedDefinition])
+
+sealed trait TyckedDefinition {
+  def meta: Option[TermMeta]
+}
+
+case class TyckedExpression(judge: Judge, meta: Option[TermMeta] = None) extends TyckedDefinition
+
+case class TyckedClass(name: String, members: Vector[TyckedDefinition], meta: Option[TermMeta] = None) extends TyckedDefinition
+
+case class TyckedFunction(name: String, params: Vector[Term], body: Judge, meta: Option[TermMeta] = None) extends TyckedDefinition
---
+++ chester-language-interpreter/common/src/main/scala/chester/tyck/stmt/ModuleTyckerInternal.scala
@@ -0,0 +1,66 @@
+package chester.tyck.stmt
+
+import chester.syntax.concrete.{Block, ModuleFile, Modules, QualifiedIDString}
+import chester.syntax.core.stmt.{TyckedBlock, TyckedDefinition, TyckedExpression, TyckedModule, TyckedModuleFile}
+import chester.tyck.{ExprTyckerInternal, Getting, LocalCtx, TyckError, TyckState}
+
+case class ModuleTyckerInternal(localCtx: LocalCtx = LocalCtx.Empty) {
+
+  // Type-check a single module file
+  def tyckModuleFile(moduleFile: ModuleFile): Getting[TyckedModuleFile] = {
+    for {
+      block <- tyckBlock(moduleFile.content)
+    } yield TyckedModuleFile(moduleFile.fileName, block)
+  }
+
+  // Type-check a vector of module files
+  def tyckModuleFiles(files: Vector[ModuleFile]): Getting[Vector[TyckedModuleFile]] = {
+    files.foldLeft(Getting.pure(Vector.empty[TyckedModuleFile])) { (acc, file) =>
+      for {
+        accFiles <- acc
+        fileTycked <- tyckModuleFile(file)
+      } yield accFiles :+ fileTycked
+    }
+  }
+
+  // Type-check a module
+  def tyckModule(id: QualifiedIDString, files: Vector[ModuleFile]): Getting[TyckedModule] = {
+    for {
+      fileTycked <- tyckModuleFiles(files)
+    } yield TyckedModule(id, fileTycked)
+  }
+
+  // Type-check a collection of modules
+  def tyckModules(modules: Modules): Getting[Vector[TyckedModule]] = {
+    modules.modules.foldLeft(Getting.pure(Vector.empty[TyckedModule])) { case (acc, (id, files)) =>
+      for {
+        accModules <- acc
+        moduleTycked <- tyckModule(id, files)
+      } yield accModules :+ moduleTycked
+    }
+  }
+
+  // Type-check a block of expressions
+  def tyckBlock(block: Block): Getting[TyckedBlock] = {
+    block.heads.foldLeft(Getting.pure(Vector.empty[TyckedDefinition])) { (acc, expr) =>
+      for {
+        accExprs <- acc
+        judge <- ExprTyckerInternal(localCtx).synthesize(expr)
+      } yield accExprs :+ TyckedExpression(judge)
+    }.map(TyckedBlock)
+  }
+}
+object ModuleTycker {
+
+  def tyckModuleFile(moduleFile: ModuleFile, state: TyckState = TyckState(), ctx: LocalCtx = LocalCtx.Empty): Either[TyckError, TyckedModuleFile] = {
+    ModuleTyckerInternal(ctx).tyckModuleFile(moduleFile).getOne(state).map(_._2)
+  }
+
+  def tyckModule(id: QualifiedIDString, files: Vector[ModuleFile], state: TyckState = TyckState(), ctx: LocalCtx = LocalCtx.Empty): Either[TyckError, TyckedModule] = {
+    ModuleTyckerInternal(ctx).tyckModule(id, files).getOne(state).map(_._2)
+  }
+
+  def tyckModules(modules: Modules, state: TyckState = TyckState(), ctx: LocalCtx = LocalCtx.Empty): Either[TyckError, Vector[TyckedModule]] = {
+    ModuleTyckerInternal(ctx).tyckModules(modules).getOne(state).map(_._2)
+  }
+}
