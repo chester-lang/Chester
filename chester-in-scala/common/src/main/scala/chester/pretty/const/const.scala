85b670478491c35eda8fcbbdc32be2c4ab21c826
澪 <mio@mio19.uk>
2024-08-09 11:51:04 +1200

    up

--- chester-in-scala/common/src/main/scala/chester/pretty/const/const.scala
+++ chester-in-scala/common/src/main/scala/chester/pretty/const/const.scala
@@ -1,8 +1,14 @@
 package chester.pretty.const
 
-import chester.pretty.doc.Color
+import chester.pretty.doc.{Color, PrettierOptionsKey}
 
 
 object Colors {
   val REPLPrompt = Color.LightCyan
 }
+
+class ColorProfile {
+  def literalColor: Color = Color.Red
+}
+
+case object ColorProfileKey extends PrettierOptionsKey[ColorProfile]
