b14041f6943d8e1883f47d3c383ea641518bb64a
澪 <mio@mio19.uk>
2024-08-10 16:04:17 +1200

    limit tests

--- chester-in-scala/common/js/src/main/scala/chester/utils/codePointIsEmoji.scala
+++ chester-in-scala/common/js/src/main/scala/chester/utils/codePointIsEmoji.scala
@@ -1,12 +1,11 @@
 package chester.utils
 
 def codePointIsEmoji(codePoint: Int): Boolean = {
-  val block = Character.UnicodeBlock.of(codePoint)
-
-  block == Character.UnicodeBlock.EMOTICONS ||
-    block == Character.UnicodeBlock.MISCELLANEOUS_SYMBOLS_AND_PICTOGRAPHS ||
-    block == Character.UnicodeBlock.TRANSPORT_AND_MAP_SYMBOLS ||
-    block == Character.UnicodeBlock.SUPPLEMENTAL_SYMBOLS_AND_PICTOGRAPHS ||
-    block == Character.UnicodeBlock.SUPPLEMENTARY_PRIVATE_USE_AREA_A ||
-    block == Character.UnicodeBlock.SUPPLEMENTARY_PRIVATE_USE_AREA_B
+  (codePoint >= 0x1F600 && codePoint <= 0x1F64F) || // Emoticons
+    (codePoint >= 0x1F300 && codePoint <= 0x1F5FF) || // Miscellaneous Symbols and Pictographs
+    (codePoint >= 0x1F680 && codePoint <= 0x1F6FF) || // Transport and Map Symbols
+    (codePoint >= 0x1F900 && codePoint <= 0x1F9FF) || // Supplemental Symbols and Pictographs
+    (codePoint >= 0xE000 && codePoint <= 0xF8FF) ||   // Supplementary Private Use Area A
+    (codePoint >= 0xF0000 && codePoint <= 0xFFFFF) || // Supplementary Private Use Area B
+    (codePoint >= 0x100000 && codePoint <= 0x10FFFF)  // Supplementary Private Use Area B continuation
 }
