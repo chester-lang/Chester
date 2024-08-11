783f0e89081ef903631e86f6e64da8daafdbdd77
澪 <mio@mio19.uk>
2024-08-11 19:00:53 +1200

    up

---
+++ chester-in-scala/common/shared/src/main/scala/chester/syntax/concrete/ResolvingModules.scala
@@ -0,0 +1,15 @@
+package chester.syntax.concrete
+
+import chester.syntax.concrete.stmt.Statement
+import chester.syntax.core.stmt.TyckedModule
+
+import scala.collection.immutable.HashMap
+
+case class ResolvingBlock(statements: Vector[Statement], expr: Option[Expr])
+
+case class ResolvingModuleFile(fileName: FilePath, content: ResolvingBlock)
+
+case class ResolvingModule(id: QualifiedIDString, resolving: Vector[ResolvingModuleFile], resolved: Option[TyckedModule])
+
+case class ResolvingModules(modules: HashMap[QualifiedIDString, ResolvingModule]) {
+}
