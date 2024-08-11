032fe920527495908ddfe6abf22aaa56934fe0c0
澪 <mio@mio19.uk>
2024-08-12 10:15:29 +1200

    up

--- chester-in-scala/common/shared/src/main/scala/chester/syntax/concrete/ResolvingModules.scala
+++ chester-in-scala/common/shared/src/main/scala/chester/syntax/concrete/ResolvingModules.scala
@@ -1,7 +1,7 @@
 package chester.syntax.concrete
 
 import chester.syntax.QualifiedIDString
-import chester.syntax.concrete.stmt.Statement
+import chester.syntax.concrete.stmt.{ExprStatement, Statement}
 import chester.syntax.core.stmt.TyckedModule
 
 import scala.collection.immutable.HashMap
@@ -13,4 +13,42 @@ case class ResolvingModuleFile(fileName: FilePath, content: ResolvingBlock)
 case class ResolvingModule(id: QualifiedIDString, resolving: Vector[ResolvingModuleFile], tycked: Option[TyckedModule])
 
 case class ResolvingModules(modules: HashMap[QualifiedIDString, ResolvingModule]) {
+}
+
+object ResolvingBlock {
+  def fromParsed(block: Block): ResolvingBlock = {
+    ResolvingBlock(
+      statements = block.heads.map(x => ExprStatement(x, x.meta)),
+      expr = block.tail
+    )
+  }
+}
+
+object ResolvingModuleFile {
+  def fromParsed(parsedFile: ParsedModuleFile): ResolvingModuleFile = {
+    ResolvingModuleFile(
+      fileName = parsedFile.fileName,
+      content = ResolvingBlock.fromParsed(parsedFile.content)
+    )
+  }
+}
+
+object ResolvingModule {
+  def fromParsed(id: QualifiedIDString, parsedFiles: Vector[ParsedModuleFile]): ResolvingModule = {
+    ResolvingModule(
+      id = id,
+      resolving = parsedFiles.map(ResolvingModuleFile.fromParsed),
+      tycked = None
+    )
+  }
+}
+
+object ResolvingModules {
+  def fromParsed(parsedModules: ParsedModules): ResolvingModules = {
+    ResolvingModules(
+      modules = parsedModules.modules.map { case (id, parsedFiles) =>
+        id -> ResolvingModule.fromParsed(id, parsedFiles)
+      }
+    )
+  }
 }
