904c937c0d76143dbf6bd1835a225cecddbe3106
澪 <mio@mio19.uk>
2024-08-17 21:05:58 +1200

    up

---
+++ chester-in-scala/common/shared/src/main/scala/chester/i18n/i18n.scala
@@ -0,0 +1,58 @@
+package chester.i18n
+
+case class Language(tag: LanguageTag, region: RegionTag) {
+  def name: String = s"${tag.name}_${region.name}"
+  override def toString: String = name
+}
+
+object Language {
+  def apply(tag: LanguageTag): Language = new Language(tag, tag.defaultRegion)
+  private val languages = LanguageTag.values
+  private val regions = RegionTag.values
+
+  def from(x: String): Option[Language] = {
+    // split by _ or -
+    val parts = x.split("[_\\-]")
+    if (parts.length == 1) {
+      languages.find(_.is(parts(0))).map(Language(_))
+    } else if (parts.length == 2) {
+      for {
+        l <- languages.find(_.is(parts(0)))
+        r <- regions.find(_.is(parts(1)))
+      } yield Language(l, r)
+    } else {
+      None
+    }
+  }
+}
+
+enum LanguageTag {
+  case EN, ZH
+
+  def name: String = toString.toLowerCase()
+
+  def is(x: String): Boolean = x.toLowerCase() == name
+
+  def defaultRegion: RegionTag = this match {
+    case EN => RegionTag.NZ
+    case ZH => RegionTag.TW
+  }
+}
+
+enum RegionTag {
+  case NZ, AU, TW, HK, US, BG
+
+  def name: String = toString.toUpperCase()
+
+  def is(x: String): Boolean = x.toUpperCase() == name
+}
+
+object RegionTag {
+
+}
+
+extension (sc: StringContext)
+  def t(args: Any*): String = {
+    sc.s(args: _*)
+  }
+
