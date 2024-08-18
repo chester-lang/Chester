4a94160b91ebea88bbfc2134986f3b5365b5e55b
澪 <mio@mio19.uk>
2024-08-18 14:41:26 +1200

    Revert "whathappended"
    This reverts commit 479c13e84170634a9403e2325d08974019ba0380.

--- chester-in-scala/common/shared/src/test/scala/chester/parser/ParserTestUtils.scala
+++ chester-in-scala/common/shared/src/test/scala/chester/parser/ParserTestUtils.scala
@@ -9,7 +9,7 @@ def parseAndCheck(input: String, expected: Expr): Unit = {
   val resultignored = Parser.parseContent("testFile", input) // it must parse with location
   Parser.parseContent("testFile", input, ignoreLocation = true) match {
     case Right(value) =>
-      assertEquals(value.descentAndApply(_.updateMeta(x=>None)), expected, s"Failed for input: $input")
+      assertEquals(value, expected, s"Failed for input: $input")
     case Left(error) =>
       fail(s"Parsing failed for input: $input ${error.message} at index ${error.index}")
   }
