ffce36ccd14497730395be782e5324f10cfb478a
澪 <mio@mio19.uk>
2024-08-20 15:48:49 +1200

    up

--- doc/render.rkt
+++ doc/render.rkt
@@ -1,5 +1,7 @@
 #lang racket
 
+; https://stackoverflow.com/questions/37234346/racket-scribble-how-to-add-a-searchbox
+
 (require (prefix-in html: scribble/html-render))
 
 (define (search:render-mixin %)
@@ -8,6 +10,7 @@
     (super-new [search-box? search-box?])))
 
 (require "index.scrbl")
+(require scribble/render)
 
 (render (list doc)
         (list "index.html")
