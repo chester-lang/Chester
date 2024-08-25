57e10e3cdaffd5f987ad693d63ccfe098a1f723d
澪 <mio@mio19.uk>
2024-08-26 11:04:40 +1200

    up

--- chester-in-scala/common/js/src/main/scala/chester/io/DefaultIO.scala
+++ chester-in-scala/common/js/src/main/scala/chester/io/DefaultIO.scala
@@ -1,16 +1,26 @@
 package chester.io
 
 import cats.Id
+import typings.node.processMod
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.Try
+import scala.concurrent.ExecutionContext.Implicits.global
 
-implicit class DefaultTryable(implicit executor: ExecutionContext) extends Tryable[Future] {
+implicit object DefaultTryable extends Tryable[Future] {
   @inline
   inline def doTry[T](IO: Future[T]): Future[Try[T]] = IO.map(Try(_)).recover(e => Try(throw e))
 }
 
-implicit class DefaultIO(implicit executor: ExecutionContext) extends IO[Future] {
+implicit object DefaultIO extends IO[Future] {
   @inline
-  inline def println(x: String): Future[Unit] = Future.successful(println(x))
+  inline def println(x: String): Future[Unit] = Future.successful(Predef.println(x))
+}
+
+implicit object DefaultSpawn extends Spawn[Future] {
+  @inline
+  inline def spawn(x: => Future[Unit]): Unit = x.recover{e =>
+    e.printStackTrace()
+    processMod.^.exit(1)
+  }
 }
