9fb61a9861abb8707f604c9848585b31d9cbbab4
澪 <mio@mio19.uk>
2024-08-26 14:35:47 +1200

    up

--- chester-in-scala/js/js/src/main/scala/chester/io/XtermTerminal.scala
+++ chester-in-scala/js/js/src/main/scala/chester/io/XtermTerminal.scala
@@ -1,5 +1,31 @@
 package chester.io
 
-class XtermTerminal {
+import chester.repl.{ReadLineResult, TerminalInfo, TerminalInit}
+
+import scala.concurrent.{Future, Promise}
+import typings.xtermXterm.mod
+
+private class InXterm(terminal: mod.Terminal, init: TerminalInit) extends InTerminal[Future] {
+  inline override def writeln(line: fansi.Str): Future[Unit] = {
+    val promise = Promise[Unit]()
+    terminal.writeln(line.render, () => {
+      promise.success(())
+      ()
+    })
+    promise.future
+  }
+
+  inline override def readline(info: TerminalInfo): Future[ReadLineResult] = ???
+
+  inline override def getHistory: Future[Seq[String]] = ???
+  
+}
+
+case class XtermTerminal(terminal: mod.Terminal) extends Terminal[Future] {
+  def runTerminal[T](init: TerminalInit, block: InTerminal[Future] ?=> Future[T]): Future[T] = {
+    val future =
+      block(using new InXterm(terminal, init))
+    future
+  }
 
 }
