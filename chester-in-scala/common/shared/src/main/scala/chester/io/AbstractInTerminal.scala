7397aac0471a0be5bc4f5a5d03512ee56e80a011
澪 <mio@mio19.uk>
2024-08-26 18:22:35 +1200

    up

--- chester-in-scala/common/shared/src/main/scala/chester/io/AbstractInTerminal.scala
+++ chester-in-scala/common/shared/src/main/scala/chester/io/AbstractInTerminal.scala
@@ -12,6 +12,9 @@ abstract class AbstractInTerminal[F[_]](using runner: Runner[F]) extends InTermi
 
   /** String could be null means EOF */
   def readALine(prompt: fansi.Str): F[String]
+  
+  /** When an implementation can only handle one line at a time in history */
+  def saveALine(line: String): F[Unit] = Runner.pure(())
 
   override def readline(info: TerminalInfo): F[ReadLineResult] = {
     def loop(prompt: fansi.Str): F[ReadLineResult] = {
@@ -22,18 +25,22 @@ abstract class AbstractInTerminal[F[_]](using runner: Runner[F]) extends InTermi
           loop(prompt) // continue reading
         } else {
           currentInputs = if (currentInputs.isEmpty) line else s"$currentInputs\n$line"
-          ParserEngine.checkInputStatus(currentInputs) match {
-            case Complete =>
-              history = history :+ currentInputs
-              val result = LineRead(currentInputs)
-              currentInputs = ""
-              Runner.pure(result)
-            case Incomplete =>
-              loop(info.continuationPrompt.render) // continue with continuation prompt
-            case Error(message) =>
-              currentInputs = ""
-              Runner.pure(StatusError(message))
-          }
+          for {
+            _ <- saveALine(line)
+            result <-
+              ParserEngine.checkInputStatus(currentInputs) match {
+                case Complete =>
+                  history = history :+ currentInputs
+                  val result = LineRead(currentInputs)
+                  currentInputs = ""
+                  Runner.pure(result)
+                case Incomplete =>
+                  loop(info.continuationPrompt)
+                case Error(message) =>
+                  currentInputs = ""
+                  Runner.pure(StatusError(message))
+              }
+          } yield result
         }
       }
     }
