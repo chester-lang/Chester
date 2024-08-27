2aecf723969850494114616abc8c2671f74a2e62
澪 <mio@mio19.uk>
2024-08-27 21:44:03 +1200

    move code

--- chester-in-scala/common/shared/src/main/scala/chester/io/IO.scala
+++ chester-in-scala/common/shared/src/main/scala/chester/io/IO.scala
@@ -69,29 +69,3 @@ extension [F[_]](_io: IO.type)(using io: IO[F]) {
   inline def chmodExecutable(inline path: io.Path): F[Unit] = io.chmodExecutable(path)
   inline def getAbsolutePath(inline path: io.Path): F[io.Path] = io.getAbsolutePath(path)
 }
-
-trait Terminal[F[_]] {
-  def runTerminal[T](init: TerminalInit, block: InTerminal[F] ?=> F[T]): F[T]
-}
-
-object Terminal {
-  inline def runTerminal[F[_], T](inline init: TerminalInit)(inline block: InTerminal[F] ?=> F[T])(using inline terminal: Terminal[F]): F[T] =
-    terminal.runTerminal(init, block)
-}
-
-
-trait InTerminal[F[_]] {
-  def writeln(line: fansi.Str): F[Unit]
-
-  def readline(info: TerminalInfo): F[ReadLineResult]
-
-  def getHistory: F[Seq[String]]
-}
-
-object InTerminal {
-  inline def writeln[F[_]](inline line: fansi.Str)(using inline terminal: InTerminal[F]): F[Unit] = terminal.writeln(line)
-
-  inline def readline[F[_]](inline info: TerminalInfo)(using inline terminal: InTerminal[F]): F[ReadLineResult] = terminal.readline(info)
-
-  inline def getHistory[F[_]](using inline terminal: InTerminal[F]): F[Seq[String]] = terminal.getHistory
-}
