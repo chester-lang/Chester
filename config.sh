2304689d5bc06ac589f65d32c03f642057089150
澪 <mio@mio19.uk>
2024-08-29 16:24:39 +1200

    up

--- config.sh
+++ config.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 # Just an empty account for downloading github packages!!!! Why do they require a token to do this!
 git config github.token "$(echo Z2hwX3VzaXlzN2VwY1IxSlY0Y0d2Skc5NFVTVDd0ZWFzWTRQRVdtNAo= | base64 -d)"
+
+
+git submodule update --init --recursive
