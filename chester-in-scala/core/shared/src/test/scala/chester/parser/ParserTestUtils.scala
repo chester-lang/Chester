f26e01464d987e3c17e7935fe12de44df242d0dd
澪 <mio@mio19.uk>
2024-09-04 18:10:48 +1200

    secretly test upickle on parser

--- chester-in-scala/core/shared/src/test/scala/chester/parser/ParserTestUtils.scala
+++ chester-in-scala/core/shared/src/test/scala/chester/parser/ParserTestUtils.scala
@@ -4,11 +4,13 @@ import chester.syntax.concrete.*
 import fastparse.*
 import munit.Assertions.{assertEquals, fail}
 import munit.FunSuite
+import upickle.default._
 
 def parseAndCheck(input: String, expected: Expr): Unit = {
   val resultignored = Parser.parseContent("testFile", input) // it must parse with location
   Parser.parseContent("testFile", input, ignoreLocation = true) match {
     case Right(value) =>
+      assertEquals(read[Expr](write[Expr](value)), value)
       assertEquals(value, expected, s"Failed for input: $input")
     case Left(error) =>
       fail(s"Parsing failed for input: $input ${error.message} at index ${error.index}")
