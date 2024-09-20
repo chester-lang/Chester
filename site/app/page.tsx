b0654cdceb4d9a822e6e006021996abdca2ede0d
澪 <mio@mio19.uk>
2024-09-21 11:52:46 +1200

    up

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect, useRef } from "react";
-import Image from "next/image";
 import { XTerm } from "@pablo-lion/xterm-react";
 import { startRepl, startReplPty, startReplReadline } from "../generated/main.js";
 import "../types/main.d.ts";
