a4206f01106bac5c2f17d4e2d122afa0933f36ab
澪 <mio@mio19.uk>
2024-09-24 20:11:54 +1200

    up

---
+++ site/favicon.sh
@@ -0,0 +1,4 @@
+convert -density 256 -background none chester-logo.svg -resize 16x16 favicon-16.png
+convert -density 256 -background none chester-logo.svg -resize 32x32 favicon-32.png
+convert -density 256 -background none chester-logo.svg -resize 48x48 favicon-48.png
+convert favicon-16.png favicon-32.png favicon-48.png favicon.ico
