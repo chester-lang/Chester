25f153d8beb0fc2530207906904259dd5f40599b
澪 <mio@mio19.uk>
2024-09-29 15:24:31 +1300

    up

--- lsp/client/src/test/extension.test.ts
+++ lsp/client/src/test/extension.test.ts
@@ -2,7 +2,14 @@ import * as assert from 'assert';
 import * as vscode from 'vscode';
 
 suite('Extension Test Suite', () => {
-  test('Sample test', async () => {
+  test('Activation Test', async () => {
+    const extension = vscode.extensions.getExtension('your-publisher-name.chester-language-support');
+    assert.ok(extension);
+    await extension!.activate();
+    assert.strictEqual(extension!.isActive, true);
+  });
+
+  test('Language Recognition Test', async () => {
     const doc = await vscode.workspace.openTextDocument({ language: 'chester', content: 'test content' });
     assert.strictEqual(doc.languageId, 'chester');
   });
