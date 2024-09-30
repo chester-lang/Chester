e39c1a4ddaac2f700a1f05a506c161cec6da8f7c
澪 <mio@mio19.uk>
2024-09-30 13:08:17 +1300

    up

--- idea-plugin/src/main/scala/chester/idea/ChesterFileTypeFactory.scala
+++
@@ -1,9 +0,0 @@
-package chester.idea
-
-import com.intellij.openapi.fileTypes.{FileTypeConsumer, FileTypeFactory}
-
-class ChesterFileTypeFactory extends FileTypeFactory {
-  override def createFileTypes(consumer: FileTypeConsumer): Unit = {
-    consumer.consume(ChesterFileType, "chester")
-  }
-}
