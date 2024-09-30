ec4a4cbd479b6d23b37c630dc85347413028dd9c
澪 <mio@mio19.uk>
2024-09-30 13:08:32 +1300

    up

--- idea-plugin/src/main/scala/chester/idea/ChesterFileType.scala
+++ idea-plugin/src/main/scala/chester/idea/ChesterFileType.scala
@@ -4,9 +4,13 @@ import com.intellij.openapi.fileTypes.LanguageFileType
 import com.intellij.openapi.util.IconLoader
 import javax.swing.Icon
 
-object ChesterFileType extends LanguageFileType(ChesterLanguage) {
+class ChesterFileType extends LanguageFileType(ChesterLanguage) {
   override def getName: String = "Chester File"
   override def getDescription: String = "Chester language file"
   override def getDefaultExtension: String = "chester"
   override def getIcon: Icon = IconLoader.getIcon("/icons/chester-icon.png", getClass)
+}
+
+object ChesterFileType {
+  val INSTANCE: ChesterFileType = new ChesterFileType()
 }
