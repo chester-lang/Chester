3580880eb99eee6028cf6a621f6c974546919fc7
澪 <mio@mio19.uk>
2024-10-01 20:00:37 +1300

    up

--- tyck/src/main/scala/chester/propagator/propagator.scala
+++ tyck/src/main/scala/chester/propagator/propagator.scala
@@ -1,10 +1,11 @@
 package chester.propagator
 
+import cats.implicits.*
 import chester.error.TyckProblem
 import chester.resolve.{SimpleDesalt, resolveOpSeq}
 import chester.syntax.concrete.*
 import chester.syntax.core.*
-import chester.tyck.{Get, LocalCtx, Reporter, SymbolTable, TyckResult, TyckResult0, TyckState, VectorReporter}
+import chester.tyck.*
 import chester.utils.propagator.*
 import chester.utils.{MutBox, reuse}
 
@@ -25,7 +26,7 @@ def resolve(expr: Expr, localCtx: LocalCtx)(using reporter: Reporter[TyckProblem
 object BaseTycker {
   type Literals = Expr & (IntegerLiteral | RationalLiteral | StringLiteral | SymbolLiteral)
 
-  case class Unify(lhs: CellId[Term], rhs: CellId[Term], meta: Option[ExprMeta], uniqId: UniqIdOf[Unify] = UniqId.generate[Unify]) extends Propagator[Ck] {
+  case class Unify(lhs: CellId[Term], rhs: CellId[Term], meta: Option[ExprMeta] = None, uniqId: UniqIdOf[Unify] = UniqId.generate[Unify]) extends Propagator[Ck] {
     override val readingCells = Set(lhs, rhs)
     override val writingCells = Set(lhs, rhs)
     override val zonkingCells = Set(lhs, rhs)
@@ -33,7 +34,10 @@ object BaseTycker {
     override def run(using state: StateAbility[Ck], more: Ck): Boolean = {
       val lhs = state.read(this.lhs)
       val rhs = state.read(this.rhs)
-      if (lhs.isDefined && rhs.isDefined && lhs.get == rhs.get) return true
+      if (lhs.isDefined && rhs.isDefined) {
+        unify(lhs.get, rhs.get)
+        return true
+      }
       return false
     }
 
@@ -61,7 +65,7 @@ object BaseTycker {
     override val zonkingCells = Set(ty)
 
     override def run(using state: StateAbility[Ck], more: Ck): Boolean = {
-      if(state.notStable(ty)) return false
+      if (state.notStable(ty)) return false
       val ty_ = state.read(this.ty).get
       val result = x match {
         case IntegerLiteral(_, _) => ty_ == IntegerType
@@ -69,7 +73,7 @@ object BaseTycker {
         case StringLiteral(_, _) => ty_ == StringType
         case SymbolLiteral(_, _) => ty_ == SymbolType
       }
-      if(result) {
+      if (result) {
         return true
       } else {
         ???
@@ -88,14 +92,114 @@ object BaseTycker {
 
   case class IsEffects(effects: CellId[Effects], uniqId: UniqIdOf[IsEffects] = UniqId.generate[IsEffects]) extends Propagator[Ck] {
     override val zonkingCells = Set(effects)
+
     override def run(using state: StateAbility[Ck], more: Ck): Boolean = state.isStable(effects)
+
     override def naiveZonk(needed: Vector[UniqIdOf[Cell[?]]])(using state: StateAbility[Ck], more: Ck): ZonkResult = {
       state.fill(effects, Effects.Empty)
       ZonkResult.Done
     }
   }
 
-  def check(expr: Expr, ty: CellId[Term], effects: CellId[Effects], localCtx: LocalCtx = LocalCtx.Empty)(using ck: Ck, state: StateAbility[Ck]): CellId[Term] = state.toId {
+  case class IsType(ty: CellId[Term], uniqId: UniqIdOf[IsType] = UniqId.generate[IsType]) extends Propagator[Ck] {
+    override val zonkingCells = Set(ty)
+
+    override def run(using state: StateAbility[Ck], more: Ck): Boolean = state.isStable(ty)
+
+    override def naiveZonk(needed: Vector[UniqIdOf[Cell[?]]])(using state: StateAbility[Ck], more: Ck): ZonkResult = {
+      state.fill(ty, AnyType0)
+      ZonkResult.Done
+    }
+  }
+
+  def newType(using ck: Ck, state: StateAbility[Ck]): CellId[Term] = {
+    val cell = OnceCell[Term]()
+    state.addCell(cell)
+    state.addPropagator(IsType(cell.uniqId))
+    cell.uniqId
+  }
+
+  case class FlatMaping[T, U](xs: Vector[CellId[T]], f: Vector[T] => U, result: CellId[U], uniqId: UniqIdOf[FlatMaping[T, U]] = UniqId.generate[FlatMaping[T, U]]) extends Propagator[Ck] {
+    override val readingCells = xs.toSet
+    override val writingCells = Set(result)
+    override val zonkingCells = Set(result)
+
+    override def run(using state: StateAbility[Ck], more: Ck): Boolean = {
+      xs.traverse(state.read(_)).map(f) match {
+        case Some(result) => {
+          state.fill(this.result, result)
+          true
+        }
+        case None => false
+      }
+    }
+
+    override def naiveZonk(needed: Vector[UniqIdOf[Cell[?]]])(using state: StateAbility[Ck], more: Ck): ZonkResult = {
+      val needed = xs.filter(state.notStable(_))
+      if (needed.nonEmpty) return ZonkResult.Require(needed)
+      val done = run
+      require(done)
+      ZonkResult.Done
+    }
+  }
+
+  def FlatMap[T, U](xs: Vector[CellId[T]])(f: Vector[T] => U)(using ck: Ck, state: StateAbility[Ck]): CellId[U] = {
+    val cell = OnceCell[U]()
+    state.addCell(cell)
+    state.addPropagator(FlatMaping(xs, f, cell.uniqId))
+    cell.uniqId
+  }
+
+  def Map[T,U](x: CellId[T])(f: T => U)(using ck: Ck, state: StateAbility[Ck]): CellId[U] = {
+    val cell = OnceCell[U]()
+    state.addCell(cell)
+    state.addPropagator(FlatMaping(Vector(x), (xs: Vector[T]) => f(xs.head), cell.uniqId))
+    cell.uniqId
+  }
+
+  def unify(t1: Term, t2: Term)(using ck: Ck, state: StateAbility[Ck]): Unit = {
+    if(t1 == t2) return
+    ???
+  }
+  def unify(t1: Term, t2: CellId[Term])(using ck: Ck, state: StateAbility[Ck]): Unit = {
+    val cell = LiteralCell[Term](t1)
+    state.addCell(cell)
+    state.addPropagator(Unify(cell.uniqId, t2))
+  }
+
+/** t is rhs, listT is lhs */
+  case class ListOf(t: CellId[Term], listT: CellId[Term], meta: Option[ExprMeta], uniqId: UniqIdOf[ListOf] = UniqId.generate[ListOf])(using localCtx: LocalCtx) extends Propagator[Ck] {
+    override val readingCells = Set(t, listT)
+    override val writingCells = Set(t, listT)
+    override val zonkingCells = Set(listT)
+
+    override def run(using state: StateAbility[Ck], more: Ck): Boolean = {
+      val t1 = state.read(this.t)
+      val listT1 = state.read(this.listT)
+      (t1, listT1) match {
+        case (Some(t1), Some(ListType(t2))) => {
+          unify(t2, t1)
+          true
+        }
+        case (_, Some(ListType(t2))) => {
+          unify(t2, t)
+          true
+        }
+        case (_, _) => false
+      }
+    }
+    override def naiveZonk(needed: Vector[UniqIdOf[Cell[?]]])(using state: StateAbility[Ck], more: Ck): ZonkResult = {
+      val t1 = state.read(this.t)
+      val listT1 = state.read(this.listT)
+      if(!t1.isDefined) return ZonkResult.Require(Vector(this.t))
+      val ty = t1.get
+      assert(listT1.isEmpty)
+      state.fill(this.listT, ListType(ty))
+      ZonkResult.Done
+    }
+  }
+
+  def check(expr: Expr, ty: CellId[Term], effects: CellId[Effects])(using localCtx: LocalCtx, ck: Ck, state: StateAbility[Ck]): CellId[Term] = state.toId {
     resolve(expr, localCtx) match {
       case expr@IntegerLiteral(value, meta) => {
         state.addPropagator(LiteralType(expr, ty, meta))
@@ -113,6 +217,12 @@ object BaseTycker {
         state.addPropagator(LiteralType(expr, ty, meta))
         SymbolTerm(value)
       }
+      case expr@ListExpr(terms, meta) => {
+        val t = newType
+        FlatMap(terms.map(check(_, t, effects))){
+          xs => ListTerm(xs)
+        }
+      }
       case expr: Expr => ???
     }
   }
@@ -123,11 +233,11 @@ type Ck = Get[TyckProblem, CkState]
 given ckToReport(using ck: Ck): Reporter[TyckProblem] = ck.reporter
 
 case class CkState(
-                      symbols: SymbolTable = Set.empty,
-                    )
+                    symbols: SymbolTable = Set.empty,
+                  )
 
 object Cker {
-  def check(expr: Expr,ty: Option[Term] = None, effects: Option[Effects] = None, localCtx: LocalCtx = LocalCtx.Empty): TyckResult[CkState, Judge] = {
+  def check(expr: Expr, ty: Option[Term] = None, effects: Option[Effects] = None, localCtx: LocalCtx = LocalCtx.Empty): TyckResult[CkState, Judge] = {
     val reporter = new VectorReporter[TyckProblem]
     implicit val get: Ck = new Get(reporter, new MutBox(CkState()))
     implicit val able: StateAbility[Ck] = new StateCells[Ck]()
@@ -138,24 +248,25 @@ object Cker {
         cell.uniqId
       }
       case None => {
-        val cell = OnceCell[Term]( None)
+        val cell = OnceCell[Term](None)
         able.addCell(cell)
         cell.uniqId
       }
     }
     val effects1: CellId[Effects] = effects match {
       case Some(effects) => {
-        val cell = LiteralCell[Effects]( effects)
+        val cell = LiteralCell[Effects](effects)
         able.addCell(cell)
         cell.uniqId
       }
       case None => {
-        val cell = OnceCell[Effects]( )
+        val cell = OnceCell[Effects]()
         able.addCell(cell)
         cell.uniqId
       }
     }
     able.addPropagator(BaseTycker.IsEffects(effects1))
+    implicit val ctx: LocalCtx = LocalCtx.Empty
     val wellTyped = BaseTycker.check(expr, ty1, effects1)
     able.naiveZonk(Vector(ty1, effects1, wellTyped))
     TyckResult0(get.getState, Judge(able.read(wellTyped).get, able.read(ty1).get, able.read(effects1).get), reporter.getReports)
