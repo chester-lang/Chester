6990506df6a0b9ce603c33d38103a9e1a6162f86
澪 <mio@mio19.uk>
2024-10-01 16:19:34 +1300

    up

--- tyck/src/main/scala/chester/propagator/propagator.scala
+++ tyck/src/main/scala/chester/propagator/propagator.scala
@@ -1,18 +1,34 @@
 package chester.propagator
 
 import chester.error.TyckProblem
-import chester.syntax.concrete.Expr
+import chester.resolve.{SimpleDesalt, resolveOpSeq}
+import chester.syntax.concrete.*
 import chester.syntax.core.*
 import chester.tyck.{LocalCtx, Reporter}
-import chester.utils.propagator.{Cell, StateAbility}
+import chester.utils.propagator.*
+import chester.utils.reuse
 
 trait Tycker[T<:Expr] {
-  def check(expr: T, ty: Option[Term], effects: Option[Effects] = None, ctx: LocalCtx = LocalCtx.Empty)(reporter: Reporter[TyckProblem], state: StateAbility[Reporter[TyckProblem]]): UniqIdOf[Cell[Judge]]
+  def check(expr: T, ty: UniqIdOf[Term], effects: UniqIdOf[Effects], localCtx: LocalCtx = LocalCtx.Empty)(using reporter: Reporter[TyckProblem], state: StateAbility[Reporter[TyckProblem]]): UniqIdOf[Cell[Judge]]
+}
+
+def resolve(expr: Expr,  localCtx: LocalCtx)(using reporter: Reporter[TyckProblem]): Expr = {
+  val result = SimpleDesalt.desugarUnwrap(expr) match {
+    case opseq: OpSeq => {
+      val result = resolveOpSeq(reporter, localCtx.ctx.operators, opseq)
+      result
+    }
+    case default => default
+  }
+  reuse(expr, result)
 }
 
 
 object BaseTycker extends Tycker[Expr] {
-  def check(expr: Expr, ty: Option[Term], effects: Option[Effects] = None, ctx: LocalCtx = LocalCtx.Empty)(reporter: Reporter[TyckProblem], state: StateAbility[Reporter[TyckProblem]]): UniqIdOf[Cell[Judge]] = {
-    ???
-  }
+  type Literals = Expr & (IntegerLiteral | RationalLiteral | StringLiteral | SymbolLiteral)
+  def check(expr: Expr, ty: UniqIdOf[Term], effects: UniqIdOf[Effects], localCtx: LocalCtx = LocalCtx.Empty)(using reporter: Reporter[TyckProblem], state: StateAbility[Reporter[TyckProblem]]): UniqIdOf[Cell[Judge]] =
+    resolve(expr, localCtx) match {
+      case IntegerLiteral(value, meta) => ???
+      case expr: Expr => ???
+    }
 }
