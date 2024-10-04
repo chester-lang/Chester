b954b1f2843f61cdf6d2672317f9571b02c657e4
澪 <mio@mio19.uk>
2024-10-04 20:26:55 +1300

    cleanup err

--- err/src/main/scala/chester/error/TyckProblem.scala
+++ err/src/main/scala/chester/error/TyckProblem.scala
@@ -45,7 +45,7 @@ sealed trait TyckProblem extends Problem derives ReadWriter {
         val locationHeader = Doc.text(t"Location") <+>
           Doc.text(t"${pos.fileName} [${pos.range.start.line + 1}:${pos.range.start.column.i + 1}] to [${pos.range.end.line + 1}:${pos.range.end.column.i + 1}]", Styling.BoldOn)
 
-        val codeBlock = Doc.group(Doc.concat(lines.map(_.end)*))
+        val codeBlock = Doc.group(Doc.concat(lines.map(_.end) *))
 
         locationHeader <|> codeBlock
 
@@ -73,50 +73,9 @@ case object ExampleWarning extends TyckWarning {
   override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Example warning"
 }
 
-case class EmptyResultsError() extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Empty Results"
-
-  override def cause: Expr = EmptyExpr
-}
-
-case class UnifyFailedError(rhs: Term, lhs: Term) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Unification failed: $rhs is not a subtype of $lhs"
-
-  override def cause: Term = rhs
-}
-
-case class UnsupportedExpressionError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Unsupported expression type ${cause.getClass.getName}"
-}
-
-case class IdentifierNotFoundError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Identifier not found"
-}
-
-case class UnexpectedStmt(cause: Stmt) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Unexpected statement"
-}
-
 import chester.syntax.concrete.qualifiedNameRW
 
 implicit val rwThis: ReadWriter[QualifiedName | String] = union2RW[Expr, String](using implicitly[ClassTag[Expr]], implicitly[ClassTag[String]], a = qualifiedNameRW.asInstanceOf[ReadWriter[Expr]], b = readwriter[String]).asInstanceOf
-
-case class FieldTypeNotFoundError(qualifiedName: QualifiedName | String) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Field type not found for $qualifiedName"
-
-  override def cause: Term | Expr = qualifiedName match {
-    case x: Term => x
-    case x: Expr => x
-    case x: String => EmptyExpr
-  }
-}
-
-case class ExpectedObjectTypeError() extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Expected an ObjectType for inheritance"
-
-  override def cause: Term | Expr = EmptyExpr
-}
-
 
 case class ExpectCase(cause: Expr) extends TyckError {
   override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"case clause must have a pattern and a return expression"
@@ -130,93 +89,17 @@ case class ExpectLambda(cause: Expr) extends TyckError {
   override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Expected a lambda expression, got "
 }
 
-case class ExpectPattern(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Expected a pattern, got "
-}
-
 case class ExpectLetDef(cause: Expr) extends TyckError {
   override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Expected a let or def statement, got "
-}
-
-case class DuplicateTelescopeArgError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Duplicate telescope argument"
-}
-
-case class UnsupportedDecorationError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Unsupported decoration "
-}
-
-case class UnsupportedVarargError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Unsupported vararg"
-}
-
-case class UnsupportedEmptyTelescopeError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Unsupported empty telescope"
-}
-
-case class InvalidNaturalError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Invalid natural number"
-}
-
-case class InvalidIntError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Invalid integer"
-}
-
-case class FieldNotFoundInObjectTypeError(field: Expr, objectType: ObjectType) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Field $field not found in object type $objectType"
-
-  override def cause: Term | Expr = field
-}
-
-case class EffectUnifyError(lhs: Effects, rhs: Judge) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Effect unification failed"
-
-  override def cause: Term | Expr = rhs.wellTyped
 }
 
 case class UnexpectedTelescope(cause: MaybeTelescope) extends TyckError {
   override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Unexpected telescope"
 }
-case class NotAFunctionError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Expected a function, but got ${cause}"
-}
 
-case class ExplicitTelescopeRequiredError(cause: MaybeTelescope) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Explicit telescope required"
-}
-case class MultipleVarargsError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Multiple varargs not allowed"
-}
-case class DuplicateArgumentError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Duplicate argument"
-}
-
-case class UnknownArgumentError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Unknown argument"
-}
-case class MissingArgumentError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Missing argument"
-}
-case class InvalidTelescopeError(cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Invalid telescope"
-}
 case class ExpectParameterList(cause: Expr) extends TyckError {
   override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc =
     t"Expected a parameter list, got ${cause}"
-}
-case class TupleArityMismatchError(provided: Int, expected: Int, cause: Expr) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc =
-    t"Tuple arity mismatch: expected $expected elements but got $provided"
-
-}
-case class MissingTypeAnnotationError(cause: Identifier) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions): Doc =
-    t"Missing type annotation for forward-referenced 'def' '${cause.name}'"
-}
-
-case class MissingBodyError(cause: Identifier) extends TyckError {
-  override def toDoc(implicit options: PrettierOptions): Doc =
-    t"Missing body expression for '${cause.name}'"
 }
 
 case class UnsupportedTermError(cause: Term) extends TyckError {
@@ -269,17 +152,18 @@ case class DuplicateDefinition(cause: Expr) extends TyckError {
 }
 
 case class FunctionCallUnificationError(
-  functionType: Term,
-  argumentTypes: Vector[Term],
-  cause: Expr
-) extends TyckError {
+                                         functionType: Term,
+                                         argumentTypes: Vector[Term],
+                                         cause: Expr
+                                       ) extends TyckError {
   override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc =
     t"Function call unification failed: expected $functionType but got $argumentTypes"
 
 }
-  case class FunctionCallArityMismatchError(expected: Int, actual: Int, cause: Expr) extends TyckError {
-    override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Function expects $expected arguments, but got $actual"
-  }
+
+case class FunctionCallArityMismatchError(expected: Int, actual: Int, cause: Expr) extends TyckError {
+  override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Function expects $expected arguments, but got $actual"
+}
 
 case class FunctionCallArgumentMismatchError(expected: Int, actual: Int, cause: Expr) extends TyckError {
   override def toDoc(implicit options: PrettierOptions = PrettierOptions.Default): Doc = t"Function expected $expected arguments, but received $actual"
