96640317b26dcf78a9becf4b4925406d014ccb58
澪 <mio@mio19.uk>
2024-10-07 14:54:30 +1300

    up

--- nodejs/src/main/scala/chester/io/DefaultSpawn.scala
+++ nodejs/src/main/scala/chester/io/DefaultSpawn.scala
@@ -2,7 +2,7 @@ package chester.io
 
 import chester.utils.io.{Runner, Spawn}
 import typings.node.processMod
-
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 import scala.util.Try
 
--- nodejs/src/main/scala/chester/parser/FilePathImplNode.scala
+++ nodejs/src/main/scala/chester/parser/FilePathImplNode.scala
@@ -1,10 +1,12 @@
 package chester.parser
 
 import typings.node.fsMod
+import typings.node.bufferMod.global.BufferEncoding
+
 
 implicit object FilePathImplNode extends FilePathImpl {
   override def readContent(fileName: String): Either[ParseError, String] =
-    Right(fsMod.readFileSync(fileName))
+    Right(fsMod.readFileSync(fileName, BufferEncoding.utf8))
 
-  override def absolute(fileName: String): String = fsMod.realpathSync(fileName)
+  override def absolute(fileName: String): String = fsMod.realpathSync(fileName, BufferEncoding.utf8)
 }
