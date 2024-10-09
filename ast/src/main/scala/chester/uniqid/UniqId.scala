6d7ec8aede84cdb3fefe5efad58a7607aee31e8a
澪 <mio@mio19.uk>
2024-10-09 15:43:40 +1300

    up

--- ast/src/main/scala/chester/uniqid/UniqId.scala
+++ ast/src/main/scala/chester/uniqid/UniqId.scala
@@ -1,8 +1,9 @@
 package chester.uniqid
 
-import upickle.default._
+import upickle.default.*
 
 import java.util.concurrent.atomic.AtomicInteger
+import scala.collection.mutable
 
 private val uniqIdCounter = AtomicInteger(0)
 
@@ -15,17 +16,20 @@ private implicit val UniqIdOffsetRW: ReadWriter[UniqIdOffset] = readwriter[java.
 extension (id: UniqIdOffset) {
   def <=(that: UniqIdOffset): Boolean = id <= that
   private[uniqid] def +(offset: Int): UniqIdOffset = id + offset
+  private[uniqid] def -(offset: UniqIdOffset): Int = id - offset
 }
 
 /** start <= x < end */
-case class UniqIdRange(start: UniqIdOffset, end: UniqIdOffset) derives ReadWriter {
+case class UniqIdRange(start: UniqIdOffset, end: UniqIdOffset)derives ReadWriter {
   require(start <= end, s"Invalid range: $start > $end")
+
+  def size: Int = end - start
 }
 
 type UniqId = UniqIdOf[Any]
 
 extension (x: UniqId) {
-  def asof[T] : UniqIdOf[T] = x.asInstanceOf[UniqIdOf[T]]
+  def asof[T]: UniqIdOf[T] = x.asInstanceOf[UniqIdOf[T]]
 }
 
 extension [T](x: UniqIdOf[T]) {
@@ -39,21 +43,21 @@ extension [T](x: UniqIdOf[T]) {
 
 private val rwUniqID: ReadWriter[UniqIdOf[?]] = readwriter[java.lang.Integer].bimap(_.toInt, _.toInt)
 
-implicit inline def rwUniqIDOf[T]: ReadWriter[UniqIdOf[T]]= rwUniqID
+implicit inline def rwUniqIDOf[T]: ReadWriter[UniqIdOf[T]] = rwUniqID
 
 trait CollectorU {
-  def apply[T](x:UniqIdOf[T]): Unit = ()
+  def apply[T](x: UniqIdOf[T]): Unit = ()
 }
 
-trait CollectUniqId  extends Any  {
+trait CollectUniqId extends Any {
   def collectU(collector: CollectorU): Unit
 }
 
 trait RerangerU {
-  def apply[T](x:UniqIdOf[T]): UniqIdOf[T] = x
+  def apply[T](x: UniqIdOf[T]): UniqIdOf[T] = x
 }
 
-trait RerangeUniqId  extends Any  {
+trait RerangeUniqId extends Any {
   def rerangeU(reranger: RerangerU): Any
 }
 
@@ -71,8 +75,16 @@ trait HasUniqId extends Any with ContainsUniqId with OnlyHasUniqId {
 
 object UniqId {
   def generate[T]: UniqIdOf[T] = uniqIdCounter.getAndIncrement()
+
+  def requireRange(size: Int): UniqIdRange = {
+    require(size > 0, s"Invalid size: $size")
+    val start = uniqIdCounter.getAndAdd(size)
+    UniqIdRange(start, start + size)
+  }
+
   def currentOffset(): UniqIdOffset = uniqIdCounter.get()
-  def captureRange[T](f: =>T): (UniqIdRange, T) = {
+
+  def captureRange[T](f: => T): (UniqIdRange, T) = {
     val start = currentOffset()
     val result = f
     val end = currentOffset()
@@ -80,4 +92,24 @@ object UniqId {
   }
 
   def is(x: Any): Boolean = x.isInstanceOf[Int] || x.isInstanceOf[Integer] || x.isInstanceOf[UniqIdOf[?]]
+
+  def calculateRange[T <: ContainsUniqId](x: T): UniqIdRange = {
+    val currentRangeCollect = new mutable.ArrayDeque[UniqId]()
+    val collecter: CollectorU = new CollectorU {
+      override def apply[T](id: UniqIdOf[T]): Unit = {
+        currentRangeCollect.append(id)
+      }
+    }
+    x.collectU(collecter)
+    UniqIdRange(currentRangeCollect.min, currentRangeCollect.max + 1)
+  }
+
+  def giveNewRange[T <: ContainsUniqId](x: T): (oldRange: UniqIdRange, newRange: UniqIdRange, result: T) = {
+    val currentRange = calculateRange(x)
+    val newRange = requireRange(currentRange.size)
+    val reranger: RerangerU = new RerangerU {
+      override def apply[T](id: UniqIdOf[T]): UniqIdOf[T] = id.rerange(currentRange, newRange)
+    }
+    (currentRange, newRange, x.rerangeU(reranger).asInstanceOf[T])
+  }
 }
