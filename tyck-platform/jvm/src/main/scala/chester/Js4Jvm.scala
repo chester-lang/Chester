437547e0fde05629c91a8323ea2ace93fde1b1a3
澪 <mio@mio19.uk>
2024-10-10 23:22:16 +1300

    fix nativeimage?

--- tyck-platform/jvm/src/main/scala/chester/Js4Jvm.scala
+++ tyck-platform/jvm/src/main/scala/chester/Js4Jvm.scala
@@ -5,12 +5,12 @@ import org.graalvm.polyglot.io.IOAccess
 
 object Js4Jvm {
   // https://www.graalvm.org/latest/reference-manual/js/Modules/
-  val context = Context
+  private val contextBuilder = Context
     .newBuilder("js")
     .allowIO(IOAccess.ALL)
     .option("js.esm-eval-returns-exports", "true")
     .option("engine.WarnInterpreterOnly", "false")
-    .build();
+  lazy val context = contextBuilder.build()
   private val source = Source.newBuilder("js", chester.generated.GeneratedJS.jsCode, "main.mjs").mimeType("application/javascript+module").build()
-  val exports = context.eval(source)
+  lazy val exports = context.eval(source)
 }
